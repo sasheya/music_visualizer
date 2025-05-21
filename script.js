991b7ef3263870370edd6f307e65b0cfd6a26650
shi <shivani.asheya@gmail.com>
2025-05-21 16:06:59 +0530

    initial music visualizer

--- script.js
+++ script.js
@@ -1,47 +1,74 @@
-    const button1 = document.getElementById('button1')
-    const audioFile = document.getElementById('audio-input')
-    const audioPlayer = document.getElementById('audio-player')
-    const audioCtx = new AudioContext()
-    console.log(audioCtx)
+// const button1 = document.getElementById('button1')
+const audioFile = document.getElementById('audio-input')
+const audioPlayer = document.getElementById('audio-player')
+let audioURL = null
 
-    const container = document.getElementById('canvas-container')
-    const canvas = document.getElementById('canvas-sound')
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-    const ctx = canvas.getContext('2d')
-    let  audioSource
-    let analyser
-    let audioURL = null
+const audioCtx = new AudioContext()
 
-    audioFile.addEventListener('change', (event) => {
-        const file = event.target.files[0]
-        if (!file) return 
+const container = document.getElementById('container')
+const canvas = document.getElementById('canvas-sound')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+const ctx = canvas.getContext('2d')
+let audioSource
+let analyser
 
-        if(audioURL) {
-            URL.revokeObjectURL(audioURL)
-        }
 
-        audioURL = URL.createObjectURL(file)
+audioFile.addEventListener('change', (event) => {
+    const file = event.target.files[0]
+    if (!file) return 
+
+    if(audioURL) {
+        URL.revokeObjectURL(audioURL)
+    }
+
+    audioURL = URL.createObjectURL(file)
         audioPlayer.src = audioURL
-    })
+})
 
-    button1.addEventListener('click', () => {
-        if (audioPlayer.src) {
-            audioPlayer.play().catch(e => console.error('Error playing audio:', e))
-            audioPlayer.addEventListener('playing', function() {
-                console.log('Audio is playing')
-            })
-            audioPlayer.addEventListener('ended', function() {
-                console.log('Audio has ended')
-            })
-        }
-        else {
-            console.alert('Upload an audio file first')
-        }
+// button1.addEventListener('click', () => {
+//     if (audioPlayer.src) {
+//         audioPlayer.play().catch(e => console.error('Error playing audio:', e))
+//         audioPlayer.addEventListener('playing', function() {
+//             console.log('Audio is playing')
+//         })
+//         audioPlayer.addEventListener('ended', function() {
+//             console.log('Audio has ended')
+//         })
+//     }
+//     else {
+//         console.alert('Upload an audio file first')
+//     }
         
-    })
+// })
+
+container.addEventListener('click', () => {
+    audioPlayer.play()
+    audioSource = audioCtx.createMediaElementSource(audioPlayer)
+    analyser = audioCtx.createAnalyser()
+    audioSource.connect(analyser)
+    analyser.connect(audioCtx.destination)
+    analyser.fftSize = 256
+    const bufferLength = analyser.frequencyBinCount
+    const dataArray = new Uint8Array(bufferLength)
+
+    const barWidth = canvas.width/bufferLength
+    let barHeight
+    let x
+
+    function animate() {
+        x = 0
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        analyser.getByteFrequencyData(dataArray) 
+        for(let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i]
+            ctx.fillStlye = 'white'
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
+            x += barWidth
+        }
+        requestAnimationFrame(animate)
+    }
+    animate()
+})
 
 
-
-
-    
