6cf5824bdc80c561675f311bd4c944e89c10cafd
shi <shivani.asheya@gmail.com>
2025-09-21 01:04:50 +0530

    chore: Remove nested Music-visualizer directory

--- Music-visualizer/colorthief.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'colorthief' {
-  export default class ColorThief {
-    getColor(img: HTMLImageElement | null, quality?: number): [number, number, number];
-    getPalette(img: HTMLImageElement | null, colorCount?: number, quality?: number): [number, number, number][];
-  }
-}
